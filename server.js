baa1f8899021e2e2deb473c0601cd139ec711bf0
unknown <adarsh4k.in@gmail.com>
2025-01-09 15:49:57 +0530

    initial commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "https://chat-app-adarsh.vercel.app",
         methods: ['GET', 'POST'],
     },
 });

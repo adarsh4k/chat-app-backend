57958458da4d52c43d323753f7c19d92d756a041
unknown <adarsh4k.in@gmail.com>
2025-01-09 13:26:06 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ['GET', 'POST'],
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+    socket.on('set_username', (username) => {
+        socket.username = username;
+    });
+    socket.on('send_message', (message) => {
+        
+        io.emit('receive_message', { username: socket.username, message});
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected:', socket.id);
+    });
+   
+    
+});
+app.get('/', (req, res) => {
+    res.send('Backend is running!');
+});
+
+
+
+
+const PORT = 5000;
+server.listen(PORT, "0.0.0.0",() => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});

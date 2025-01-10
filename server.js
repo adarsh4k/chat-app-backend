4748c6348199d7c7877a1df0c3dcc3c5172763ad
unknown <adarsh4k.in@gmail.com>
2025-01-10 13:31:28 +0530

    removed slash

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://chat-app-adarsh.vercel.app/", // Replace with your frontend URL
+        origin: "https://chat-app-adarsh.vercel.app", 
         methods: ["GET", "POST"],
     },
 });
